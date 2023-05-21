3752d011bcae25e32f8f5f99a8227e661642ee4c
Hayk Atshemyan <123772647+Atshemyan@users.noreply.github.com>
2023-05-21 16:32:24 +0400

    Update string.hpp

--- string.hpp
+++ string.hpp
@@ -1,14 +1,16 @@
 class string {
 public:
     // Constructor
-    string(const char* str) {
+    string(const char* str)
+    {
         size = strlen(str);
         data = new char[size + 1];
         memcpy(data, str, size + 1);
     }
 
     // Move constructor
-    string(string&& other) {
+    string(string&& other)
+    {
         size = other.size;
         data = other.data;
         other.data = nullptr;
@@ -16,7 +18,8 @@ public:
     }
 
     // Destructor
-    ~string() {
+    ~string()
+    {
         delete[] data;
         data = nullptr;
         size = 0;
